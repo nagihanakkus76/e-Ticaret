1e3ac28bc7bf9e43ce1556acc574c7dc1b870c62
nagihan.akkus <nagihan.akkus.76@gmail.com>
2025-08-22 16:48:35 +0300

    chore(ui): initial setup

---
+++ apps/ui/src/app.config.ts
@@ -0,0 +1,17 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideHttpClient(),
+    provideRouter(appRoutes),
+  ],
+};
